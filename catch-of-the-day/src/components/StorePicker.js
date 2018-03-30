cdbbe075d8066b5ffe1ad2d3fbc167fde1eb3fa6
Kotoriii <cmackm@hotmail.com>
2018-03-30 17:05:07 +0200

    understanding state

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+	myInput = React.createRef();
+
+	goToStore = (event) => {
+		event.preventDefault();
+		const storeName = this.myInput.value.value; //react thing + JS .value
+		this.props.history.push(`/store/${storeName}`); //react router history
+	}
+
 	render() {
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore} >
 				<h2>Please enter a Store</h2>
-				<input type="text" required placeholder="Store Name" />
+				<input type="text" required placeholder="Store Name" ref={this.myInput} defaultValue={getFunName()} />
 				<button type="submit">Visit Store</button>
 			</form>
 		)
